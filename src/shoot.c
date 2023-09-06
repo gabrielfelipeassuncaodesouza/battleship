c7417ec38e83bd694f572bcd46b6d26b289783e6
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-06 01:08:37 -0300

    refactor the shoot function

--- src/shoot.c
+++ src/shoot.c
@@ -1,29 +1,48 @@
 #include "shoot.h"
+#include "human.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+//TODO: change the type of the value of the struct to all int
 
 int isValid(char c) {
     return c != ERROR && c != ASSERT;
 }
 
+char coordinates(char board[][TAM], chute_t c) {
+  int x = (int)(c.x - 'a'), y = c.y;
+
+  return board[x][y];
+}
+
+chute_t iaChute(char board[][TAM]) {
+  chute_t chute;
+
+  do {
+    chute.x = 'A'+ rand() % (TAM-1);
+    chute.y = rand() % TAM;
+  } while(!isValid(coordinates(board, chute)));
+
+  return chute;
+}
+
 int shoot(char board[][TAM]) {
-    int x, y;
+    chute_t s = iaChute(board);
+    char result = coordinates(board, s);
 
-    do {
-        x = rand() % TAM;
-        y = rand() % TAM;
-    } while(!isValid(board[x][y]));
-
-    if(board[x][y] == WATER) {
+    if(result == WATER) {
         printf("\nAGUA!!\n\n");
-        board[x][y] = ASSERT;
+        board[(int)(s.x - 'a')][s.y] = ASSERT;
     }
 
-    else if(board[x][y] == SHIP) {
+    else if(result == SHIP) {
         printf("\nBOMBA!!\n\n");
-        board[x][y] = ERROR;
+        board[(int)(s.x - 'a')][s.y] = ERROR;
         return 1;
     }
+    else {
+      printf("\nError\n\n");
+    } 
 
     return 0;
 }
