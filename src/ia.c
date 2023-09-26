40059841f1504e0e330a86dcf4302f909286647c
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-26 19:07:00 -0300

    improve the hunt and target algorithm to don't miss any square

--- src/ia.c
+++ src/ia.c
@@ -1,51 +1,31 @@
 #include "board.h"
 #include "ia.h"
+#include "queue.h"
 #include "ships.h"
 #include "shoot.h"
 
 #include <stdlib.h>
 
-chute_t iaChute(element_t board[][TAM], chute_t neighbours[], int i, int hitted) {
+chute_t iaChute(element_t board[][TAM], queue_t** hits) {
   chute_t chute;
-  
-  int tries = 0;
 
   do {
-    if(tries >= 8) hitted = 0;
-
-    if(!hitted) {
+    if(*hits == NULL) {
       chute.x = rand() % TAM;
       chute.y = rand() % TAM;
     }
     else {
-      tries++;
-      chute = neighbours[i++];
+      chute = (*hits)->hit;
+      rem(hits);
     }
   } while(isPositionShooted(coordinates(board, chute)));
 
   return chute;
 }
 
-void getNeighbours(element_t board[][TAM], chute_t neigh[], chute_t pos) {
-
-  neigh[0] = (chute_t){ pos.x, pos.y+1 };
-  neigh[1] = (chute_t){ pos.x+1, pos.y };
-  neigh[2] = (chute_t){ pos.x, pos.y-1 };
-  neigh[3] = (chute_t){ pos.x-1, pos.y };
-
-  if(pos.y == TAM - 1) { //se estiver na parede direita
-    neigh[0] = (chute_t){ -1, -1 };
-  }
-
-  if(pos.x == TAM - 1) { // se estiver em baixo
-    neigh[1] = (chute_t){ -1, -1 };
-  }
-
-  if(pos.y == 0) { //se estiver na parede esquerda
-    neigh[2] = (chute_t){ -1, -1 };
-  }
-
-  if(pos.x == 0) { // se estiver em cima
-    neigh[3] = (chute_t){ -1, -1 };
-  }  
+void getNeighbours(element_t board[][TAM], queue_t** hits, queue_t** tail, chute_t pos) {
+  add(hits, tail, (chute_t){ pos.x, pos.y+1 });
+  add(hits, tail, (chute_t){ pos.x+1, pos.y });
+  add(hits, tail, (chute_t){ pos.x, pos.y-1 });
+  add(hits, tail, (chute_t){ pos.x-1, pos.y }); 
 }
